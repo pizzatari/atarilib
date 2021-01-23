d36b94e6b265336d55d253efca5b16482e7854c2
baconpizza <baconpizza@users.noreply.github.com>
2021-01-22 22:15:47 -0800

    Added atarilib top level file.

---
+++ include/atarilib.h
@@ -0,0 +1,23 @@
+; -----------------------------------------------------------------------------
+; Author:   Edward Gilmour
+; Date:     January 2021
+; Desc:     Common definition files used for Blackjack Theta VIII and Battle
+;           for Proton Atari 2600 games.
+; Notes:    atarilib directory should be placed in the parent directory
+;           containing the game project files.
+; -----------------------------------------------------------------------------
+
+    include "../atarilib/include/bankswitch.h"
+    include "../atarilib/include/bits.h"
+    include "../atarilib/include/debug.h"
+    include "../atarilib/include/draw.h"
+    include "../atarilib/include/io.h"
+    include "../atarilib/include/position.h"
+    include "../atarilib/include/task.h"
+    include "../atarilib/include/time.h"
+    include "../atarilib/include/util.h"
+    include "../atarilib/include/video.h"
+
+    ; TODO: move these dasm files elsewhere
+    include "../atarilib/include/macro.h"
+    include "../atarilib/include/vcs.h"
--- include/io.h
+++ include/io.h
@@ -1,17 +1,26 @@
+; Objects
+OBJ_P0          = 0     ; player 0
+OBJ_P1          = 1     ; player 1
+OBJ_M0          = 2     ; missile 0
+OBJ_M1          = 3     ; missile 1
+OBJ_BL          = 4     ; ball
+
+; Console switches
 SWITCH_DIFF1    = %10000000
 SWITCH_DIFF0    = %01000000
 SWITCH_BW       = %00001000
 SWITCH_SELECT   = %00000010
 SWITCH_RESET    = %00000001
 
-JOY1_RIGHT      = %00001000
-JOY1_LEFT       = %00000100
-JOY1_DOWN       = %00000010
-JOY1_UP         = %00000001
-JOY0_RIGHT      = JOY1_RIGHT << 4
-JOY0_LEFT       = JOY1_LEFT  << 4
-JOY0_DOWN       = JOY1_DOWN  << 4
-JOY0_UP         = JOY1_UP    << 4
-
+; Controllers
 JOY_FIRE        = %10000000
 
+JOY1_RIGHT      = %10000000
+JOY1_LEFT       = %01000000
+JOY1_DOWN       = %00100000
+JOY1_UP         = %00010000
+
+JOY1_RIGHT      = JOY0_RIGHT >> 4
+JOY1_LEFT       = JOY0_LEFT  >> 4
+JOY1_DOWN       = JOY0_DOWN  >> 4
+JOY1_UP         = JOY0_UP    >> 4
